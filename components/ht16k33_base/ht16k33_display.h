f4f5d3ca9077345afdb0d68c7c683017c7abedd6
Alca7raz <113485718+Alca7raz@users.noreply.github.com>
2026-08-16 17:14:06 -0700

    Update ht16k33_display.h

--- components/ht16k33_base/ht16k33_display.h
+++ components/ht16k33_base/ht16k33_display.h
@@ -28,10 +28,12 @@ class HT16K33BaseDisplay : public PollingComponent, public i2c::I2CDevice {
   float get_setup_priority() const override;
   void add_secondary_display(i2c::I2CDevice *display) { this->displays_.push_back(display); }
   void set_scroll(bool scroll) { this->scroll_ = scroll; }
+  void set_swap_bits(bool swap_bits) { this->swap_bits_ = swap_bits; }
   void set_continuous(bool continuous) { this->continuous_ = continuous; }
   void set_scroll_speed(unsigned long scroll_speed) { this->scroll_speed_ = scroll_speed; }
   void set_scroll_dwell(unsigned long scroll_dwell) { this->scroll_dwell_ = scroll_dwell; }
   void set_scroll_delay(unsigned long scroll_delay) { this->scroll_delay_ = scroll_delay; }
+  void set_size(unsigned long size) { this->size_ = size; }
   void update() override;
   //// Clear display
   void set_brightness(float level);
@@ -57,15 +59,18 @@ class HT16K33BaseDisplay : public PollingComponent, public i2c::I2CDevice {
   virtual uint16_t read_character_(uint8_t c) const = 0;
   virtual uint16_t decimal_point_mask_() const = 0;
   virtual bool supports_colon_() const = 0;
+  uint16_t swapBits(uint16_t n, uint p1, uint p2);
 
   std::vector<i2c::I2CDevice *> displays_ {this};
   std::function<void(HT16K33BaseDisplay &)> writer_;
   bool scroll_ {false};
   bool continuous_ {false};
+  bool swap_bits_ {false};
   unsigned long scroll_speed_ {250};
   unsigned long scroll_dwell_ {2000};
   unsigned long scroll_delay_ {750};
   unsigned long last_scroll_ {0};
+  unsigned long size_ {4};
   std::vector<uint8_t> buffer_;
   int offset_ {0};
   uint8_t brightness_ = 16;
